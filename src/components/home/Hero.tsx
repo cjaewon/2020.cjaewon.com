2095bc25d7726cf24d419f9d9709a61f0791ed46
cjaewon <jwn4492@outlook.com>
2020-09-22 09:44:57 +0900

    UPDATE: hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,48 +1,45 @@
 import React, { useEffect } from 'react';
 import { css } from '@emotion/core';
-import anime from 'animejs';
+
 
 import CjaewonLogo from '../../static/svg/cjaewon-logo.svg';
 
 const Hero: React.FC<{}> = () => {
-  useEffect(() => {
-    anime({
-      targets: '.hero-svg path',
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: 'easeInOutSine',
-      duration: 1500,
-      delay: function(el, i) { return i * 250 },
-      direction: 'alternate',
-      loop: true,
-    });
-  }, []);
-
   return (
     <div css={styles}>
-      <div className="">
+      <div className="hero__center">
         <CjaewonLogo />
+        <h2>The FullStack Dev.</h2>
       </div>
     </div>
   );
 };
 
 const styles = css`
-  text-align: center;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   p {
     font-size: 2rem;
   }
 
-  .hero-svg {
-    margin-top: 14rem;
+  .hero__center {
+    text-align: center;
 
-    path {
-      stroke-dasharray: 1000;
-      stroke-dashoffset: 0;
+    h2 {
+      margin-top: 0.1rem;
+      margin-bottom: 5rem;
     }
   }
 
-
+  .hero__svg {
+  }
 `;
 
 export default Hero;
