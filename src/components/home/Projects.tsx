37fa4f44e8352d35f6c318eb1347b57eb8dc2db7
cjaewon <jwn4492@outlook.com>
2020-09-28 11:26:09 +0900

    Update Project UI

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -2,15 +2,72 @@ import React from 'react';
 import { css } from '@emotion/core';
 
 const Projects: React.FC<{}> = () => {
+  const code = '> node cjaewon-projects.js';
+
+  //   급식봇          gomokuio                  letterbot
+
+  //   cjaewon.com   letterbot-webhookletter   more...
+
+  // `;
+
   return (
     <section css={styles}>
+      <div className="projects__highlight">
+        <pre>
+        <code className="language-bash" data-lang="bash">
+          {code}
 
+          <div className="projects__list">
+            <div><a href="/projects/mealbot">급식봇</a></div>
+            <div><a href="/projects/gomokuio">gomokuio</a></div>
+            <div><a href="/projects/letterbot">letterbot</a></div>
+            <div><a href="/projects/cjaewon.com">cjaewon.com</a></div>
+            <div><a href="/projects/letterbot-webhookletter">letterbot-webhookletter</a></div>
+            <div><a href="https://github.com/cjaewon">more...</a></div>
+          </div>
+
+        </code>
+        </pre>
+      </div>
     </section>
   );
 };
 
 const styles = css`
   height: 100vh;
+
+  max-width: 760px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+
+  .projects__highlight {
+    width: 100%;
+    color: #f8f8f2;
+    background-color: #272822;
+    border-radius: 4px;
+    padding: 1.25rem 1.925rem;
+
+    code {
+      font-family: 'Fira Code', monospace;
+    }
+  }
+
+  .projects__list {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(2, 1fr); 
+    margin-top: 0.75rem;
+  }
+
+  a {
+    color: white;
+
+    &:hover {
+      color: #00CED1;
+      font-style: italic;
+    }
+  }
 `;
 
 export default Projects;
--- src/lib/styles/GlobalStyles.tsx
+++ src/lib/styles/GlobalStyles.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { css, Global } from '@emotion/core';
 
 const styles = css`
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500&family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
 
   html, body {
     margin: 0;
